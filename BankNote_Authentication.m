1174d57c0610b831c0c422ea36dfd3e1e3b51eb9
Anshuman Singh <41586858+Anshuman-37@users.noreply.github.com>
2021-11-01 16:13:49 +0000

    Classification Loading data

---
+++ BankNote_Authentication.m
@@ -0,0 +1,11 @@
+clear all;
+close all;
+
+% KNN for Classification
+
+%Loading Data 
+ T = readtable('C:\Users\ppxas6\Desktop\OneDrive_2021-11-01\ML Computer\BankNote_Authentication.csv')
+
+% Dividing Data into X and Y for the prediction
+
+
